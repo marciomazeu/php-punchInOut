22ff286bedcf156ed62c8b277f115e8b86c4d657
marciomazeu <mazeu911@gmail.com>
2024-03-29 22:12:20 -0400

    autentication page

--- public/index.php
+++ public/index.php
@@ -2,24 +2,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2).'/src/config/config.php');
-require_once(dirname(__FILE__, 2).'/src/models/User.php');
-
-
-$user = new User(['name'=>'Marcio', 'email'=>'marcio@hotmail.com']);
-//print_r($user);
-$user->email = 'alterado@gmail.com';
-
-//echo "<br><br><br>";
-//print_r($user->email);
-//echo "<br><br><br>";
-
-
-//echo User::getSelect(['id' => 1],'email, name');
-//echo User::getSelect(['name' => 'Chaves'],'email, id');
-
-var_dump(User::get(['name'=>'Chaves']));
-
-foreach(User::get([], 'name') as $user){
-    echo $user->name;
-    echo '<br>';
-}
+require_once(dirname(__FILE__, 2).'/src/views/login.php');
---
+++ src/views/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>Punch In And Out</title>
+</head>
+<body>
+    <form class="form-login" action="#" method="post">
+        <div class="login-card card">
+            <div class="card-header">
+                <h3>Punch In N' Out</h3>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email" class="label">E-mail</label>
+                    <input type="email" autofocus 
+                    class="input-email"
+                    name="email"
+                    id="email"
+                    placeholder="Informe o e-mail"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="password" class="label">Senha</label>
+                    <input type="password" 
+                    class="input-password"
+                    name="password"
+                    id="password"
+                    placeholder="Informe o password"/>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn-entrar">Entrar</button>
+            </div>
+        </div>
+
+
+
+    </form>
+</body>
+</html>
